0d62608d124e86be1a93be57b4cc8dbf5a54624a
jwblau2 <jeremy@abundantly.com>
2023-07-06 19:05:15 -0600

    added questions variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 // Quiz questions
 const questions = [
-    {
-      question: "What is the capital city of France?",
-      choices: ["London", "Paris", "Madrid", "Rome"],
-      answer: 1
-    },
+  {
+    question: "What is the capital city of France?",
+    choices: ["London", "Paris", "Madrid", "Rome"],
+    answer: 1
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    choices: ["Jupiter", "Mars", "Venus", "Saturn"],
+    answer: 0
+  },
+  {
+    question: "What is the powerhouse of the cell?",
+    choices: ["Mitochondria", "Nucleus", "Ribosome", "Golgi Apparatus"],
+    answer: 0
+  }
+];
