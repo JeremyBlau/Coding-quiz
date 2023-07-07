683ea09124e07a4c4d9632775b879bd32910a2f6
jwblau2 <jeremy@abundantly.com>
2023-07-07 16:42:42 -0600

    added css formatting to the buttons and choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,6 @@
 // Quiz questions
 const questions = [
   {
-    question: "What is the capital city of France?",
-    choices: ["London", "Paris", "Madrid", "Rome"],
-    answer: 1
-  },
-  {
-    question: "Which is the largest planet in our solar system?",
-    choices: ["Jupiter", "Mars", "Venus", "Saturn"],
-    answer: 0
-  },
-  {
-    question: "What is the powerhouse of the cell?",
-    choices: ["Mitochondria", "Nucleus", "Ribosome", "Golgi Apparatus"],
-    answer: 0
-  },
-  {
     question: "What does CSS stand for?",
     choices: ["Cascading Style Sheets", "Computer Science Syntax", "Creative Styling System", "Coding Style Standards"],
     answer: 0
